1c60c651dfa5055c488bda63d5dbbaf3841113e7
Mohammad Ahmad <49246417+shahim92@users.noreply.github.com>
2019-04-21 15:51:08 -0400

    Add files via upload

---
+++ scanner/WhileLoop.java
@@ -0,0 +1,18 @@
+package scanner;
+
+public class WhileLoop {
+	//while loop basically a structure of a java code  which allow to run a block of java code.
+	//While loop checks the condition 1st whether true or false then it goes to next statement 
+public static void main(String[] args) {
+	int i = 1;
+	//here i is index variable which is int (May be String or boolean)
+	while(i<10) {
+		// here while is a condition which is i is less than or equal 10
+		System.out.println(i);
+		i++;
+		//i++ means incremental i
+		//it can be i-- for decremental i	
+}
+}
+}
+// 
