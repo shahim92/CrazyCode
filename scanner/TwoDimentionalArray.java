a587a544374441fbc9b97bc9a2232447f959d8c1
Mohammad Ahmad <49246417+shahim92@users.noreply.github.com>
2019-04-22 17:31:36 -0400

    Add files via upload

---
+++ scanner/TwoDimentionalArray.java
@@ -0,0 +1,18 @@
+package scanner;
+
+public class TwoDimentionalArray {
+	public static void main(String[] args) {
+		
+		int [] [] numberGrid = {
+				{1,2,3},
+				{4,5,6},
+				{7,8,9},
+				{0}
+		};
+		System.out.println(numberGrid[3][0]);
+	}
+	
+
+}
+
+//2 dimentional array is a array within an array.
